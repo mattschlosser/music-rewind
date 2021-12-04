17a1f499be17a9378c66ceefe4cc4306164685bb
Matt Schlosser <mattschlosser@users.noreply.github.com>
2021-12-03 17:04:40 -0700

    Remove logo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,14 +2,6 @@
   <v-container>
     
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
 
       <v-col class="mb-4">
         <h2 class="display-2 font-weight-bold mb-3">
